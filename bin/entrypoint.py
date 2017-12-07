81c607a48963bc66499b394c47650f0caae69405
Henry Royal <dev@hwr.io>
2017-12-07 06:54:25 -0500

    batch inserting into the reports table

--- bin/entrypoint.py
+++ bin/entrypoint.py
@@ -83,63 +83,64 @@ def main(target_rssd, hbase_reinit, hbase_host, hbase_port,
             continue
 
         for institution in reporters:
-            #TODO populate institution table
-            rssd = int(institution[ID_RSSD])
-            if rssd != target_rssd and target_rssd != RSSD_WILDCARD:
-                continue
+            with loader.report_table.batch() as report_table:
+                #TODO populate institution table
+                rssd = int(institution[ID_RSSD])
+                if rssd != target_rssd and target_rssd != RSSD_WILDCARD:
+                    continue
 
-            unicode_sdf_facsimile = Transformer.bytes_to_unicode(
-                ffiec.call_report_facsimile(period, institution)
-            )
-            facsimile = Transformer.sdf_to_dictreader(unicode_sdf_facsimile)
+                unicode_sdf_facsimile = Transformer.bytes_to_unicode(
+                    ffiec.call_report_facsimile(period, institution)
+                )
+                facsimile = Transformer.sdf_to_dictreader(unicode_sdf_facsimile)
 
-            row_key = bytes('{rssd}-{period}'.format(rssd=rssd, period=period), 'utf-8')
-            for key in institution:
-                if not institution[key]:
-                    institution[key] = ''
-                if isinstance(institution[key], bytes):
-                    institution[key] = str(institution[key])
-                if isinstance(institution[key], (str)):
-                    institution[key] = institution[key].strip()
-                if isinstance(institution[key], (int, float)):
-                    institution[key] = str(institution[key])
+                row_key = bytes('{rssd}-{period}'.format(rssd=rssd, period=period), 'utf-8')
+                for key in institution:
+                    if not institution[key]:
+                        institution[key] = ''
+                    if isinstance(institution[key], bytes):
+                        institution[key] = str(institution[key])
+                    if isinstance(institution[key], (str)):
+                        institution[key] = institution[key].strip()
+                    if isinstance(institution[key], (int, float)):
+                        institution[key] = str(institution[key])
 
-                column_key = bytes('Institution:{}'.format(key.strip().lower().replace(' ', '_')), 'utf-8')
-                logging.debug('{row} Institution:{col} = {value} '.format(value=institution[key],
-                                                                          col=column_key,
-                                                                          row=period))
+                    column_key = bytes('Institution:{}'.format(key.strip().lower().replace(' ', '_')), 'utf-8')
+                    logging.debug('{row} Institution:{col} = {value} '.format(value=institution[key],
+                                                                              col=column_key,
+                                                                              row=period))
 
-                loader.report_table.put(row_key, {column_key: institution[key]})
+                    report_table.put(row_key, {column_key: institution[key]})
 
-            logging.info('loaded RSSD# {rssd} from {period} into report::Institution'.format(rssd=rssd,
-                                                                                             period=period))
+                logging.info('loaded RSSD# {rssd} from {period} into report::Institution'.format(rssd=rssd,
+                                                                                                 period=period))
 
-            for item in facsimile:
-                formatted_mdrm = item[MDRM].upper()
-                for key in item:
-                    formatted_key = key.strip().lower().replace(' ', '_')
-                    if not item[key]:
-                        item[key] = ''
-                    if isinstance(item[key], bytes):
-                        item[key] = str(item[key], 'utf-8')
-                    if isinstance(item[key], (str)):
-                        item[key] = item[key].strip()
-                    if isinstance(item[key], (int, float)):
-                        item[key] = str(item[key])
+                for item in facsimile:
+                    formatted_mdrm = item[MDRM].upper()
+                    for key in item:
+                        formatted_key = key.strip().lower().replace(' ', '_')
+                        if not item[key]:
+                            item[key] = ''
+                        if isinstance(item[key], bytes):
+                            item[key] = str(item[key], 'utf-8')
+                        if isinstance(item[key], (str)):
+                            item[key] = item[key].strip()
+                        if isinstance(item[key], (int, float)):
+                            item[key] = str(item[key])
 
-                    formatted_value = bytes(item[key], 'utf-8')
-                    column_key = bytes('CallReport:{mdrm}:{key}'.format(mdrm=formatted_mdrm,
-                                                                        key=formatted_key), 'utf-8')
+                        formatted_value = bytes(item[key], 'utf-8')
+                        column_key = bytes('CallReport:{mdrm}:{key}'.format(mdrm=formatted_mdrm,
+                                                                            key=formatted_key), 'utf-8')
 
-                    logging.debug('{row} CallReport:{mdrm}:{key} = {value}'.format(row=row_key,
-                                                                                   mdrm=item[MDRM],
-                                                                                   key=formatted_key,
-                                                                                   value=formatted_value))
+                        logging.debug('{row} CallReport:{mdrm}:{key} = {value}'.format(row=row_key,
+                                                                                       mdrm=item[MDRM],
+                                                                                       key=formatted_key,
+                                                                                       value=formatted_value))
 
-                    loader.report_table.put(row_key, {column_key: formatted_value})
+                        report_table.put(row_key, {column_key: formatted_value})
 
-            logging.info('loaded RSSD# {rssd} from {period} into report::CallReport'.format(rssd=rssd,
-                                                                                            period=period))
+                logging.info('loaded RSSD# {rssd} from {period} into report::CallReport'.format(rssd=rssd,
+                                                                                                period=period))
         logging.info('job complete')
         job_complete_timestamp = datetime.now()
         job_runtime = (job_complete_timestamp - job_start_timestamp)
